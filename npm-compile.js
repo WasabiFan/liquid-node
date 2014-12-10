dc8c19bab6506fdbd620b17000916d784e9dac06
WasabiFan <wasabifan@outlook.com>
2014-12-09 23:02:38 -0800

    Replace compile command with npm script to allow for cross-platform compiling

---
+++ npm-compile.js
@@ -0,0 +1,25 @@
+//Script to emulate this command (for cross-platform usage by npm):
+//rm -rf lib && coffee --output lib --map --compile src
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+deleteFolderRecursive('./lib');
+exec('coffee --output lib --map --compile src');
+
+
+function deleteFolderRecursive(path) {
+    var files = [];
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path);
+        files.forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
